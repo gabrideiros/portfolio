22cc02aab3bfdaaa381dad0b9881e73afe4c481b
Gabriel Medeiros <gabrielmedeirosbrito12082004@gmail.com>
2025-07-10 20:22:33 -0300

    update header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,8 +9,8 @@ import Skills from "./skills";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br bg-background">
+      <Header />
       <div className="mx-auto px-4 sm:px-6 max-w-7xl">
-        <Header />
         <Hero />
         <Projects />
         <Skills />
