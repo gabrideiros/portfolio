6c3751895ac2f2ec97738847e5e8e6f8bae94ead
Gabriel Medeiros <gabrielmedeirosbrito12082004@gmail.com>
2025-07-10 18:34:40 -0300

    fix margin

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,12 +9,14 @@ import Skills from "./skills";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br bg-background">
-      <Header />
-      <Hero />
-      <Projects />
-      <Skills />
-      <About />
-      <Contact />
+      <div className="mx-auto px-4 sm:px-6 max-w-7xl">
+        <Header />
+        <Hero />
+        <Projects />
+        <Skills />
+        <About />
+        <Contact />
+      </div>
       <Footer />
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio",
+  //base: "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
